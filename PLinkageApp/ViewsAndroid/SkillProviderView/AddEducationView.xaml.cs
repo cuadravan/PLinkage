19b7f1a6e9cae4a870ab8114a6e595431e72b565
buddy <buddy@LAPTOP-OPQ3CUD6>
2025-10-25 00:26:40 +0800

    Created ViewSkill View AddEducation View UpdateEducation View

---
+++ PLinkageApp/ViewsAndroid/SkillProviderView/AddEducationView.xaml.cs
@@ -0,0 +1,9 @@
+namespace PLinkageApp.ViewsAndroid.SkillProviderView;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PLinkageApp/ViewsAndroid/SkillProviderView/ViewSkillView.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace PLinkageApp.ViewsAndroid
+{
+    public partial class ViewSkillView : ContentPage
+    {
+        public ViewSkillView()
+        {
+            InitializeComponent();
+        }
+    }
+}
